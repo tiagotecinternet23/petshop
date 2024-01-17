a57e56becb76232e69e60dfdec91b85b21bdb0d0
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-17 09:56:56 -0300

    Favicon, meta tag author, pt-br

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="/images/favicon.png"
+          type="image/png"
+          sizes="256x256"
+        />
+        <meta name="author" content="Tiago B. dos Santos" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
