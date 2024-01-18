c7623efd70e191f2b82715fcfb32e61ee283eb3f
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-18 09:08:36 -0300

    StyledHeader (parcial)

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,21 +1,40 @@
 import Link from "next/link";
 import Menu from "./Menu";
 import Image from "next/image";
+import styled from "styled-components";
 
 export default function Cabecalho() {
   return (
-    <header>
+    <StyledHeader>
       <h1>
-        <Image
-          src="/images/logo.png"
-          width={48}
-          height={48}
-          alt="Patinha dentro de um coração"
-        />
-
-        <Link href="/">PetShop</Link>
+        <Link href="/">
+          <Image
+            src="/images/logo.png"
+            width={48}
+            height={48}
+            alt="Patinha dentro de um coração"
+          />
+          PetShop
+        </Link>
       </h1>
       <Menu />
-    </header>
+    </StyledHeader>
   );
 }
+
+const StyledHeader = styled.header`
+  background-color: #f7f7f7;
+  box-shadow: var(--sombra-box);
+
+  h1 a {
+    text-decoration: none;
+    color: var(--cor-primaria);
+    background-color: var(--cor-primaria-fundo);
+    padding: 0.5rem 1.2rem;
+    border-radius: var(--borda-arredondada);
+    font-size: 1.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
