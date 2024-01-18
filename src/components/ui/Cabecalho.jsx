586667ca08ffe157568f777ee12d03b41b526f15
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-18 08:45:26 -0300

    Adição do logo usando Image

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
 import Menu from "./Menu";
+import Image from "next/image";
 
 export default function Cabecalho() {
   return (
     <header>
       <h1>
+        <Image
+          src="/images/logo.png"
+          width={48}
+          height={48}
+          alt="Patinha dentro de um coração"
+        />
+
         <Link href="/">PetShop</Link>
       </h1>
       <Menu />
