1759b5f0391e97588ddb109f3b4657118eb868c1
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-18 08:38:29 -0300

    Adição de Link ao h1

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import Menu from "./Menu";
 
 export default function Cabecalho() {
   return (
     <header>
-      <h1>PetShop</h1>
+      <h1>
+        <Link href="/">PetShop</Link>
+      </h1>
       <Menu />
     </header>
   );
