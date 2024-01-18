e018992e8ccc8d5d67f04274126f73f57655f235
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-18 09:16:25 -0300

    StyledNav (parcial)

--- src/components/ui/Menu.jsx
+++ src/components/ui/Menu.jsx
@@ -1,12 +1,26 @@
 import Link from "next/link";
+import styled from "styled-components";
 
 export default function Menu() {
   return (
-    <nav>
+    <StyledNav>
       <Link href="/">Blog</Link>
       <Link href="/produtos">Produtos</Link>
       <Link href="/sobre">Sobre</Link>
       <Link href="/contato">Contato</Link>
-    </nav>
+    </StyledNav>
   );
 }
+
+const StyledNav = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    text-decoration: none;
+    background-color: var(--cor-primaria-fundo);
+    color: var(--cor-primaria);
+    padding: 0.8rem 1rem;
+  }
+`;
