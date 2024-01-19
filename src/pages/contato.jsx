a6b766c57efc552f5415adc93c8f08ed847c9e23
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-19 09:10:46 -0300

    Estrutura do form de contato

--- src/pages/contato.jsx
+++ src/pages/contato.jsx
@@ -1,3 +1,4 @@
+import Container from "@/components/ui/Container";
 import Head from "next/head";
 import styled from "styled-components";
 
@@ -14,6 +15,32 @@ export default function Contato() {
       </Head>
       <StyledContato>
         <h2>Fale Conosco</h2>
+
+        <Container>
+          <form action="" method="post">
+            <div>
+              <label htmlFor="nome">Nome: </label>
+              <input type="text" name="nome" id="nome" />
+            </div>
+            <div>
+              <label htmlFor="email">E-mail: </label>
+              <input type="email" name="email" id="email" />
+            </div>
+            <div>
+              <label htmlFor="mensagem">Mensagem:</label>
+              <textarea
+                maxLength={500}
+                name="mensagem"
+                id="mensagem"
+                cols="30"
+                rows="8"
+              ></textarea>
+            </div>
+            <div>
+              <button type="submit">Enviar mensagem</button>
+            </div>
+          </form>
+        </Container>
       </StyledContato>
     </>
   );
