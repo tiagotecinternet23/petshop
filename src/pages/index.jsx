bafbe546c8e151aca32b760564157de591b80d45
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-22 08:43:43 -0300

    Correção do desafio

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import Link from "next/link";
 import styled from "styled-components";
 import arrayPosts from "./api/array-posts";
+import ListaPosts from "@/components/ListaPosts";
 
 export default function Home() {
   return (
@@ -16,58 +16,11 @@ export default function Home() {
       </Head>
       <StyledHome>
         <h2>Pet Notícias</h2>
-        <StyledListaPosts>
-          {arrayPosts.map((post) => {
-            return (
-              <article key={post.id}>
-                <Link href="">
-                  <h3> {post.titulo} </h3>
-                  <p> {post.subtitulo} </p>
-                </Link>
-              </article>
-            );
-          })}
-        </StyledListaPosts>
+        <ListaPosts posts={arrayPosts} />
       </StyledHome>
     </>
   );
 }
-
-const StyledListaPosts = styled.div`
-  article {
-    background-color: #f7f7f7;
-    padding: 1rem;
-    margin-bottom: 1rem;
-    box-shadow: var(--sombra-box);
-    border-radius: var(--borda-arredondada);
-    transition: transform 200ms;
-
-    & a {
-      text-decoration: none;
-      color: black;
-
-      &:hover,
-      &:focus {
-        color: #0066ff;
-      }
-    }
-  }
-
-  article:hover {
-    cursor: pointer;
-    transform: scale(1.05);
-  }
-
-  @media screen and (min-width: 500px) {
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-
-    article {
-      width: 49%;
-    }
-  }
-`;
 
 const StyledHome = styled.section`
   h2::before {
