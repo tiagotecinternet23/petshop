f353c155e4bd61bb49a016792c793dfc8959b18e
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-23 11:31:27 -0300

    Configuração do getStaticProps

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,10 +1,29 @@
 import Container from "@/components/ui/Container";
 import Head from "next/head";
 import styled from "styled-components";
+import serverApi from "../api/server";
 
 export async function getStaticProps({ params }) {
   const { id } = params;
   console.log(id);
+
+  try {
+    const resposta = await fetch(`${serverApi}/posts/${id}`);
+
+    if (!resposta.ok) {
+      throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
+    }
+
+    const dados = await resposta.json();
+
+    return {
+      props: {
+        post: dados,
+      },
+    };
+  } catch (error) {
+    console.error("Deu ruim: " + error.message);
+  }
 }
 
 export async function getStaticPaths() {
