f15f3c0eb346eb007860ef5cd831e7e86bc08301
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-23 11:04:52 -0300

    Estrutura da página

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,3 +1,24 @@
+import Container from "@/components/ui/Container";
+import Head from "next/head";
+import styled from "styled-components";
+
 export default function Post() {
-  return <h2>Página Post</h2>;
+  return (
+    <>
+      <Head>
+        <title>Título do post.... - PetShop</title>
+        <meta name="description" content="Descrição do post..." />
+      </Head>
+
+      <StyledPost>
+        <h2>Título do post....</h2>
+        <Container>
+          <h3>Categoria do post...</h3>
+          <p>Descrição do post...</p>
+        </Container>
+      </StyledPost>
+    </>
+  );
 }
+
+const StyledPost = styled.article``;
