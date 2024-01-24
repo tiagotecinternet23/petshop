05617b0089811346774a545624ee2ae26e4c0181
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-24 10:29:09 -0300

    Ajuste do key usando indice

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -50,8 +50,8 @@ export default function Home({ posts, categorias }) {
         <h2>Pet Notícias</h2>
 
         <div>
-          {categorias.map((categoria) => {
-            return <button>{categoria}</button>;
+          {categorias.map((categoria, indice) => {
+            return <button key={indice}>{categoria}</button>;
           })}
         </div>
 
