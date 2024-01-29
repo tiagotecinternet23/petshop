568b02156b21eada3f14cf69bfb154f32e718f48
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-29 10:45:29 -0300

    Criação do ListaCategorias

---
+++ src/components/ListaCategorias.jsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+export default function ListaCategorias() {
+  return (
+    <StyledCategorias>
+      {categorias.map((categoria, indice) => {
+        return (
+          <button
+            className={categoria === categoriaAtiva ? "ativo" : ""}
+            onClick={filtrar}
+            key={indice}
+          >
+            {categoria}
+          </button>
+        );
+      })}
+
+      {filtroAtivo && (
+        <button onClick={limparFiltro} className="limpar">
+          Limpar filtro
+        </button>
+      )}
+    </StyledCategorias>
+  );
+}
+
+const StyledCategorias = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 1rem 0;
+  flex-wrap: wrap;
+
+  button {
+    text-transform: capitalize;
+    border: none;
+    background-color: var(--cor-secundaria-fundo);
+    color: #f7f7f7;
+    padding: 0.5rem;
+    border-radius: var(--borda-arredondada);
+
+    &:hover,
+    &:focus {
+      background-color: var(--cor-secundaria-fundo-hover);
+      cursor: pointer;
+    }
+
+    &.ativo {
+      background-color: var(--cor-primaria-fundo);
+    }
+  }
+
+  .limpar {
+    background-color: gray;
+    &:hover {
+      background-color: slategray;
+    }
+    &::before {
+      content: "🧹 ";
+    }
+  }
+`;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
+import { useState } from "react";
 import styled from "styled-components";
 import ListaPosts from "@/components/ListaPosts";
-import { useState } from "react";
+import ListaCategorias from "@/components/ListaCategorias";
 import serverApi from "./api/server";
 
 export async function getStaticProps() {
@@ -74,68 +75,12 @@ export default function Home({ posts, categorias }) {
       <StyledHome>
         <h2>Pet Notícias</h2>
 
-        <StyledCategorias>
-          {categorias.map((categoria, indice) => {
-            return (
-              <button
-                className={categoria === categoriaAtiva ? "ativo" : ""}
-                onClick={filtrar}
-                key={indice}
-              >
-                {categoria}
-              </button>
-            );
-          })}
-
-          {filtroAtivo && (
-            <button onClick={limparFiltro} className="limpar">
-              Limpar filtro
-            </button>
-          )}
-        </StyledCategorias>
-
+        <ListaCategorias />
         <ListaPosts posts={listaDePosts} />
       </StyledHome>
     </>
   );
 }
-
-const StyledCategorias = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  margin: 1rem 0;
-  flex-wrap: wrap;
-
-  button {
-    text-transform: capitalize;
-    border: none;
-    background-color: var(--cor-secundaria-fundo);
-    color: #f7f7f7;
-    padding: 0.5rem;
-    border-radius: var(--borda-arredondada);
-
-    &:hover,
-    &:focus {
-      background-color: var(--cor-secundaria-fundo-hover);
-      cursor: pointer;
-    }
-
-    &.ativo {
-      background-color: var(--cor-primaria-fundo);
-    }
-  }
-
-  .limpar {
-    background-color: gray;
-    &:hover {
-      background-color: slategray;
-    }
-    &::before {
-      content: "🧹 ";
-    }
-  }
-`;
 
 const StyledHome = styled.section`
   h2::before {
