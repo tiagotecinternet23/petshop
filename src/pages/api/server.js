a83d203364d419757ff3a80c756fcc81594737ac
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-30 09:36:31 -0300

    Update para url do firebase

--- src/pages/api/server.js
+++ src/pages/api/server.js
@@ -1,8 +1,3 @@
 // api/server.js
-
-const porta = 2112;
-const endereco = "10.20.46.20";
-
-const serverApi = `http://${endereco}:${porta}`;
-
+const serverApi = `https://petshop-tiago-2024-default-rtdb.firebaseio.com/`;
 export default serverApi;
