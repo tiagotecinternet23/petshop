9d31119e8dea1a4b98c0fa71ed32b013c9d79c22
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-30 11:59:53 -0300

    Fetch dos dados e router pra redirect

--- src/pages/contato.jsx
+++ src/pages/contato.jsx
@@ -2,12 +2,31 @@ import Container from "@/components/ui/Container";
 import Head from "next/head";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
+import serverApi from "./api/server";
+import { useRouter } from "next/router";
 
 export default function Contato() {
   const { register, handleSubmit } = useForm();
+  let router = useRouter();
 
-  const enviarContato = (dados) => {
-    console.log(dados);
+  const enviarContato = async (dados) => {
+    const { nome, email, mensagem } = dados;
+
+    const opcoes = {
+      method: "POST",
+      body: JSON.stringify({ nome, email, mensagem }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    };
+
+    try {
+      await fetch(`${serverApi}/contatos.json`, opcoes);
+      alert("Dados enviados!");
+      router.push("/");
+    } catch (error) {
+      console.error("Deu ruim no envio: " + error.message);
+    }
   };
 
   return (
