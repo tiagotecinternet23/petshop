29a7ea8d45071277421f9848ef86596abad011ed
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2024-01-31 10:25:46 -0300

    Testes

--- public/sw.js
+++
@@ -1,3 +0,0 @@
-/* Monitoramento do evento fetch por meio do próprio navegador/dispositivo. 
-É algo auto-gerenciável, por isso, não alteramos nada. */
-self.addEventListener("fetch", function (event) {});
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,20 +3,6 @@ import Layout from "@/components/ui/Layout";
 import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/sw.js", { scope: "/" })
-          .then((registration) => {
-            console.log("Service Worker registrado com sucesso:", registration);
-          })
-          .catch((error) => {
-            console.error("Erro ao registrar o Service Worker:", error);
-          });
-      });
-    }
-  }, []);
   return (
     <Layout>
       <Component {...pageProps} />
